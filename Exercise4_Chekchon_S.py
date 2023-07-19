0958b942a3a69605de809430a1801141fb60eb00
TTNNNNTT <tonnam.study@gmail.com>
2023-07-19 19:42:55 +0700

    Exercise4
    Homework

---
+++ Exercise4_Chekchon_S.py
@@ -0,0 +1,9 @@
+English = 85.5
+Business = 90
+Computer = 89.5
+Programming = 91.5
+print('---Your Score---')
+print('Foundation English : ',English)
+print('General Business : ',Business)
+print('Introduction to Computer Systems : ',Computer)
+print('Computer Programming : ',Programming)
