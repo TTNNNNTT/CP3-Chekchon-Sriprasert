81fceba814e1f5a682002ffaf102646e0c43bd9d
TTNNNNTT <tonnam.study@gmail.com>
2023-07-20 00:32:01 +0700

    Exercise 9

---
+++ Exercise9_Chekchon_S.py
@@ -0,0 +1,7 @@
+username = input("username : ")
+password = input("password : ")
+while username != "TONNAM" or password != "212224":
+    print("Please try again")
+    username = input("username : ")
+    password = input("password : ")
+print("Login Success :D")
