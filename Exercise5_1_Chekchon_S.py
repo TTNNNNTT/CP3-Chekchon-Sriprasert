31b573a53713324f1c80a8108b2128daa8b05553
TTNNNNTT <tonnam.study@gmail.com>
2023-07-19 22:21:01 +0700

    Exercise5
    Homework

---
+++ Exercise5_1_Chekchon_S.py
@@ -0,0 +1,6 @@
+x = int(input("First Number : "))
+y = int(input("Second Number : "))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",x/y)
---
+++ Exercise5_2_Chekchon_S.py
@@ -0,0 +1,4 @@
+s = float(input("Distance (km) : "))
+t = float(input("Time (h) : "))
+v = (s/t)
+print(v,"km/h")
